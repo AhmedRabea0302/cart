0539730d5923f6509ef792344c024ce8efa0ff86
Fadloovich <arabea169@gmail.com>
2023-02-16 18:25:45 +0200

    Added Update Total Cart items

--- src/context.js
+++ src/context.js
@@ -29,6 +29,20 @@ const AppProvider = ({ children }) => {
   const decreaseItem = (id) => {
     dispatch({ type: "DECREASE_ITEM", payload: id });
   };
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
   return (
     <AppContext.Provider
       value={{
--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,20 @@
 import { act } from "react-dom/test-utils";
 
 const reducer = (state, action) => {
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
-  } else if (action.type === "REMOVE_ITEM") {
+  }
+  if (action.type === "REMOVE_ITEM") {
     const newItems = state.cart.filter((item) => item.id !== action.payload);
     return { ...state, cart: newItems };
-  } else if (action.type === "INCREASE_ITEM") {
+  }
+  if (action.type === "INCREASE_ITEM") {
     const tempCart = state.cart.map((cartItem) => {
       if (cartItem.id === action.payload) {
         return { ...cartItem, amount: cartItem.amount++ };
@@ -15,7 +23,8 @@ const reducer = (state, action) => {
     });
 
     return { ...state, cart: tempCart };
-  } else if (action.type === "DECREASE_ITEM") {
+  }
+  if (action.type === "DECREASE_ITEM") {
     const tempCart = state.cart
       .map((cartItem) => {
         if (cartItem.id === action.payload) {
@@ -26,6 +35,20 @@ const reducer = (state, action) => {
       .filter((item) => item.amount !== 0);
 
     return { ...state, cart: tempCart };
+  }
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.amount += amount;
+        cartTotal.total = itemTotal;
+        return cartTotal;
+      },
+      { total: 0, amount: 0 }
+    );
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
   }
   return state;
 };
